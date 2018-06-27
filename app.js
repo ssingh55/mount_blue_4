bd3666eadbd4ec793bc103a97f41c8df1a06d6a2
Shubham Singh <shubham.singh@mountblue.io>
2018-06-27 18:45:33 +0530

    adding event listener

--- app.js
+++ app.js
@@ -1,61 +1,84 @@
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
+// require timer.js
+var memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
-Array.prototype.memory_tile_shuffle = function(){
-    var i = this.length, j, temp;
-    while(--i > 0){
-        j = Math.floor(Math.random() * (i+1));
+var count = 0;
+
+Array.prototype.memory_tile_shuffle = function() {
+    var i = this.length,
+        j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
         temp = this[j];
         this[j] = this[i];
         this[i] = temp;
     }
 }
-function newBoard(){
-	tiles_flipped = 0;
-	var output = '';
-    memory_array.memory_tile_shuffle();
-	for(var i = 0; i < memory_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
-	}
-	document.getElementById('memory_board').innerHTML = output;
+//restart button
+function reload(){
+	window.location.reload();
 }
-function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory_values.length < 2){
-		tile.style.background = '#FFF';
-		tile.innerHTML = val;
-		if(memory_values.length == 0){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-		} else if(memory_values.length == 1){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-			if(memory_values[0] == memory_values[1]){
-				tiles_flipped += 2;
-				// Clear both arrays
-				memory_values = [];
-            	memory_tile_ids = [];
-				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
-					alert("Board cleared... generating new board");
-					document.getElementById('memory_board').innerHTML = "";
-					newBoard();
-				}
-			} else {
-				function flip2Back(){
-				    // Flip the 2 tiles back over
-				    var tile_1 = document.getElementById(memory_tile_ids[0]);
-				    var tile_2 = document.getElementById(memory_tile_ids[1]);
-				    tile_1.style.background = 'url(small-rocket-ship-silhouette.png) no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(small-rocket-ship-silhouette.png) no-repeat';
-            	    tile_2.innerHTML = "";
-				    // Clear both arrays
-				    memory_values = [];
-            	    memory_tile_ids = [];
-				}
-				setTimeout(flip2Back, 700);
-			}
-		}
-	}
+
+//tiles flipped
+const board = document.querySelector('#memory_board');
+console.log(board);
+board.addEventListener('click', function(e) {
+    if (e.target.className == 'tile_count') {
+        count++;
+         document.getElementById('tries').innerHTML = "No. of tries: "+ count;
+    }
+});
+
+function newBoard() {
+    tiles_flipped = 0;
+    var output = '';
+    memory_array.memory_tile_shuffle();
+    for (var i = 0; i < memory_array.length; i++) {
+        output += '<div class="tile_count" id="tile_' + i + '" onclick="memoryFlipTile(this,\'' + memory_array[i] + '\')"></div>';
+    }
+    document.getElementById('memory_board').innerHTML = output;
+    document.getElementById("tries").innerHTML = "No. of tries: "+ 0;
+  //  timer();
+}
+
+function memoryFlipTile(tile, val) {
+    if (tile.innerHTML == "" && memory_values.length < 2) {
+        tile.style.background = '#FFF';
+        tile.innerHTML = val;
+        if (memory_values.length == 0) {
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+        } else if (memory_values.length == 1) {
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+            if (memory_values[0] == memory_values[1]) {
+                tiles_flipped += 2;
+                // Clear both arrays
+                memory_values = [];
+                memory_tile_ids = [];
+                // Check to see if the whole board is cleared
+                if (tiles_flipped == memory_array.length) {
+                    // alert("Board cleared... generating new board");
+                    document.getElementById('complete').innerHTML = "Completed";
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+            } else {
+                function flip2Back() {
+                    // Flip the 2 tiles back over
+                    var tile_1 = document.getElementById(memory_tile_ids[0]);
+                    var tile_2 = document.getElementById(memory_tile_ids[1]);
+                    tile_1.style.background = 'url(small-rocket-ship-silhouette.png) no-repeat';
+                    tile_1.innerHTML = "";
+                    tile_2.style.background = 'url(small-rocket-ship-silhouette.png) no-repeat';
+                    tile_2.innerHTML = "";
+                    // Clear both arrays
+                    memory_values = [];
+                    memory_tile_ids = [];
+                }
+                setTimeout(flip2Back, 700);
+            }
+        }
+    }
 }
---
+++ timer.js
@@ -0,0 +1,21 @@
+function timer(){
+var input = {
+    year: 0,
+    month: 0,
+    day: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+};
+
+var timestamp = new Date(input.year, input.month, input.day,
+input.hours, input.minutes, input.seconds);
+
+var interval = 1;
+
+setInterval(function () {
+    timestamp = new Date(timestamp.getTime() + interval * 1000);
+    // document.getElementById('countdown').innerHTML = timestamp.getDay() + 'd:' + timestamp.getHours() + 'h:' + timestamp.getMinutes() + 'm:' + timestamp.getSeconds() + 's';
+    document.getElementById('countdown').innerHTML = timestamp.getHours() + 'h:' + timestamp.getMinutes() + 'm:' + timestamp.getSeconds() + 's';
+}, Math.abs(interval) * 1000);
+}
